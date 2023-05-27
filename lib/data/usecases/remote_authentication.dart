a20201ad81b14e5c9617c9cb6e528530172e3577
Helena Brito Dias <62960218+helenabd@users.noreply.github.com>
2023-05-27 11:52:34 -0300

    ensure RemoteAuthentication returns an Account on 200

--- lib/data/usecases/remote_authentication.dart
+++ lib/data/usecases/remote_authentication.dart
@@ -9,13 +9,14 @@ class RemoteAuthentication {
     required this.httpClient,
     required this.url,
   });
-  Future<void> auth(AuthenticationParams params) async {
+  Future<AccountEntity> auth(AuthenticationParams params) async {
     try {
-      await httpClient.request(
+      final httpResponse = await httpClient.request(
         url: url,
         method: 'post',
         body: RemoteAuthenticationParams.fromDomain(params).toJson(),
       );
+      return AccountEntity.fromJson(httpResponse);
     } on HttpError catch (error) {
       throw error == HttpError.unauthorized
           ? DomainError.invalidCredentials
