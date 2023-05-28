7d37ea6250e18659f6a9b63c7c6e5cb2d592eba7
Helena Brito Dias <62960218+helenabd@users.noreply.github.com>
2023-05-28 09:14:11 -0300

    ensure HttpAdapter calls post with correct URL

---
+++ test/infra/http/http_adapter_test.dart
@@ -0,0 +1,43 @@
+import 'package:faker/faker.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mocktail/mocktail.dart';
+import 'package:http/http.dart';
+
+import '../mocks/mocks.dart';
+
+class HttpAdapter {
+  final Client client;
+
+  HttpAdapter(this.client);
+
+  Future<void> request({
+    required String url,
+    required String method,
+  }) async {
+    await client.post(Uri.parse(url));
+  }
+}
+
+void main() {
+  late HttpAdapter sut;
+  late ClientSpy client;
+  late String url;
+
+  setUp(() {
+    client = ClientSpy();
+    sut = HttpAdapter(client);
+  });
+
+  setUpAll(() {
+    url = faker.internet.httpUrl();
+    registerFallbackValue(Uri.parse(url));
+  });
+
+  group('post', () {
+    test('Should call post with correct values', () async {
+      await sut.request(url: url, method: 'post');
+
+      verify(() => client.post(Uri.parse(url)));
+    });
+  });
+}
