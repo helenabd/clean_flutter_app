bbff722e50d00ff58574b5d9f7e53307fda384d3
Helena Brito Dias <62960218+helenabd@users.noreply.github.com>
2023-05-28 13:37:49 -0300

    ensure HttpAdapter throws BadRequestError on 400 without data

--- test/infra/http/http_adapter_test.dart
+++ test/infra/http/http_adapter_test.dart
@@ -78,8 +78,17 @@ void main() {
       expect(response, null);
     });
 
-    test('Should return BadRequestError if post returns 400', () async {
+    test('Should return BadRequestError if post returns 400 without data',
+        () async {
       client.mockPost(400, body: '');
+
+      final future = sut.request(url: url, method: 'post');
+
+      expect(future, throwsA(HttpError.badRequest));
+    });
+
+    test('Should return BadRequestError if post returns 400', () async {
+      client.mockPost(400);
 
       final future = sut.request(url: url, method: 'post');
 
