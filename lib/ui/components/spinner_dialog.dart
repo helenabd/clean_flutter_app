934cb9f1a274df1c5017266d88303fe9398ff1ec
Helena Brito Dias <62960218+helenabd@users.noreply.github.com>
2023-06-12 12:03:44 -0300

    move SpinnerDialog to its own file

---
+++ lib/ui/components/spinner_dialog.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+void showLoading({required BuildContext context}) {
+  showDialog(
+    context: context,
+    barrierDismissible: false,
+    builder: (context) {
+      return SimpleDialog(
+        children: [
+          Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: const [
+              CircularProgressIndicator(),
+              SizedBox(height: 10),
+              Text(
+                'Aguarde ...',
+                textAlign: TextAlign.center,
+              )
+            ],
+          )
+        ],
+      );
+    },
+  );
+}
+
+void hideLoading({required BuildContext context}) {
+  if (Navigator.canPop(context)) {
+    Navigator.of(context).pop();
+  }
+}
