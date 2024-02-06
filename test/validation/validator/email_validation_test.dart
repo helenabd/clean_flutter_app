b9b2a96bdb534256e2a37fbdb210cf3bb61a4ba6
Helena Brito Dias <62960218+helenabd@users.noreply.github.com>
2024-02-06 10:08:05 -0300

    move duplicated code to the setup method

--- test/validation/validator/email_validation_test.dart
+++ test/validation/validator/email_validation_test.dart
@@ -2,29 +2,29 @@ import 'package:clean_flutter_app/validation/protocols/field_validation.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 class EmailValidation implements FieldValidation {
+  @override
   final String field;
 
   EmailValidation(this.field);
 
+  @override
   String? validate(String? value) {
     return null;
   }
 }
 
 void main() {
+  late EmailValidation sut;
+
+  setUp(() {
+    sut = EmailValidation('any_field');
+  });
+
   test('Should return null if email is empty', () {
-    final sut = EmailValidation('any_field');
-
-    final error = sut.validate('');
-
-    expect(error, null);
+    expect(sut.validate(''), null);
   });
 
   test('Should return null if email is null', () {
-    final sut = EmailValidation('any_field');
-
-    final error = sut.validate(null);
-
-    expect(error, null);
+    expect(sut.validate(null), null);
   });
 }
